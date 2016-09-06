d472008e813feafbd532569de2f26fea2910cb9c
macbirch <macbirch@gmail.com>
2016-09-06 15:27:35 -0600

    9/6 update

---
+++ views/Deptlist.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Contents: City Departments</h1>
+<a href="http://www.w3schools.com">Visit W3Schools</a>
+<a href = "#Animal-Welfare">Animal Welfair</a>
+<a href = "#Aviation Department">Aviation Department</a>
+<a href = "#CityClerksOffice">City Clerk's Office</a>
+<a href = "#CityCouncil">City Council</a>
+<a href = "#CivilianPoliceOversightAgency">Civilian Police Oversight Agency</a>
+<a href = "#CulturalServicesDepartment">Cultural Services Department</a>
+<a href = "#EconomicDevelopment">Economic Development</a>
+<a href = "#EmergencyManagementOffice">Emergency Management Office</a>
+<a href = "#EnvironmentalHealthDepartment">Environmental Health Department</a>
+<a href = "#FamilyCommunityServicesDepartment">Family & Community Services Department</a>
+<a href = "#FinanceAdministrativeServicesDepartment">Finance & Administrative Services Department</a>
+<a href = "#FinanceAdministrativeServicesDepartment">Finance & Administrative Services Department</a>
+<a href = "#FireDepartment">Fire Department</a>
+<a href = "#HumanResourcesDepartment">Human Resources Department</a>
+<a href = "#InternalAuditOffice">Internal Audit Office</a>
+<a href = "#LegalDepartment">Legal Department</a>
+<a href = "#ManagementBudgetOffice">Management & Budget Office</a>
+<a href = "#Mayor's Office">Mayor's Office</a>
+<a href = "#MunicipalDevelopmentDepartment">Municipal Development Department</a>
+<a href = "#ParksRecreationDepartment">Parks & Recreation Department</a>
+<a href = "#PlanningDepartment">Planning Department</a>
+<a href = "#PoliceDepartment">Police Department</a>
+<a href = "#SeniorAffairsDepartment">Senior Affairs Department</a>
+<a href = "#SolidWasteDepartment">Solid Waste Department</a>
+<a href = "#TransitDepartment">Transit Department</a>
+<a href = "#TechnologyInnovationDepartment">Technology & Innovation Department</a>
+<a href = "#SeniorAffairsDepartment">Senior Affairs Department</a>
+
+  </body>
+</html>
